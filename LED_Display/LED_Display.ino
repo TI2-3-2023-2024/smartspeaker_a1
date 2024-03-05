e135fa2ee2371596ce277e0991f4dd3b25fc91ca
NielsB1 <n.buijs@student.avans.nl>
2024-03-05 11:04:15 +0100

    i2c slave on esp32

--- LED_Display/LED_Display.ino
+++ LED_Display/LED_Display.ino
@@ -1,19 +1,23 @@
-/*
- * This ESP32 code is created by esp32io.com
- *
- * This ESP32 code is released in the public domain
- *
- * For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-ws2812b-led-strip
- */
-
 #include <Adafruit_NeoPixel.h>
+#include <Wire.h>
+#include <WireSlave.h>
+#include "driver/i2c.h"
+
+#define SDA_PIN 21
+#define SCL_PIN 22
+#define I2C_SLAVE_ADDR 0x04
 
 #define PIN_WS2812B 12  // The ESP32 pin GPIO16 connected to WS2812B
 #define NUM_PIXELS 256  // The number of LEDs (pixels) on WS2812B LED strip
 
 Adafruit_NeoPixel ws2812b(NUM_PIXELS, PIN_WS2812B, NEO_GRB + NEO_KHZ800);
 
+
 void setup() {
+  Wire.begin(4);                // join i2c bus with address #4
+  Wire.onReceive(receiveEvent); // register event
+  Serial.begin(9600);           // start serial for output
+
   ws2812b.begin();  // initialize WS2812B strip object (REQUIRED)
   ws2812b.clear();  // set all pixel colors to 'off'. It only takes effect if pixels.show() is called
 
@@ -23,7 +27,17 @@ void setup() {
   turn_on_led(5,5, 0, 255, 0);
 
   turn_on_row(0, 255, 0, 0);
-  turn_below_point_on(3, 6, 0, 0, 255);
+}
+
+void receiveEvent(int howMany)
+{
+  while(1 < Wire.available()) // loop through all but the last
+  {
+    char c = Wire.read(); // receive byte as a character
+    Serial.print(c);         // print the character
+  }
+  int x = Wire.read();    // receive byte as an integer
+  Serial.println(x);         // print the integer
 }
 
 void turn_on_led(int x, int y, int r, int g , int b){
@@ -36,7 +50,7 @@ void turn_on_led(int x, int y, int r, int g , int b){
     pixel += y;
   }
     ws2812b.setPixelColor(pixel, ws2812b.Color(r, g, b));  // it only takes effect if pixels.show() is called
-    ws2812b.setBrightness(100); // set brightness of the pixels
+    ws2812b.setBrightness(50); // set brightness of the pixels
     ws2812b.show();     
 }
 
@@ -53,5 +67,4 @@ void turn_below_point_on(int x, int y, int r, int g, int b){
 }
 
 void loop() {
-
 }
