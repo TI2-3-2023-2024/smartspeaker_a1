11b7c2554a2aafe2c075863d539c03f9a5801486
NielsB1 <n.buijs@student.avans.nl>
2024-03-18 10:38:22 +0100

    task delete when closing radio(BIG BUG INCLUDED)

--- main/menu.c
+++ main/menu.c
@@ -13,8 +13,7 @@ static i2c_dev_t pcf8574;
 
 static hd44780_t lcd;
 
-
-//pages
+// pages
 menu_page current_page;
 
 menu_page main_page;
@@ -27,6 +26,7 @@ menu_page radio_play_page;
 radio_station selected_station;
 static radio_station stations[3];
 static int station_index = 0;
+TaskHandle_t radio_task_handle;
 
 static const uint8_t char_data[] = {
     0x04, 0x0e, 0x0e, 0x0e, 0x1f, 0x00, 0x04, 0x00,
@@ -64,13 +64,12 @@ void input_menu()
 }
 
 /*
-* Function: initialiseer de inhoud van de radio pagina
-* Parameters: None
-* Returns: None
-*/
-void radio_page_init(){
-    current_page = radio_selection_page;
-
+ * Function: initialiseer de inhoud van de radio pagina
+ * Parameters: None
+ * Returns: None
+ */
+void radio_page_init()
+{
     stations[0] = (radio_station){1, "Sunrise", "http://185.66.249.48:8006/stream?type=.mp3"};
     stations[1] = (radio_station){2, "Jumbo FM", "https://playerservices.streamtheworld.com/api/livestream-redirect/JUMBORADIO.mp3"};
     stations[2] = (radio_station){3, "Slam FM", "https://stream.slam.nl/web10_mp3"};
@@ -80,6 +79,8 @@ void radio_page_init(){
 
 void radio_selection_menu()
 {
+    current_page = radio_selection_page;
+
     hd44780_clear(&lcd);
     hd44780_gotoxy(&lcd, 0, 0);
     hd44780_puts(&lcd, "SELECT RADIO STATION");
@@ -90,12 +91,13 @@ void radio_selection_menu()
     hd44780_gotoxy(&lcd, 7, 1);
     hd44780_puts(&lcd, stations[station_index].radio_name);
 
-    if ((station_index + 1) < (sizeof(stations) / sizeof(stations[0]))){
-    hd44780_gotoxy(&lcd, 0, 2);
-    hd44780_puts(&lcd, "NEXT: ");
+    if ((station_index + 1) < (sizeof(stations) / sizeof(stations[0])))
+    {
+        hd44780_gotoxy(&lcd, 0, 2);
+        hd44780_puts(&lcd, "NEXT: ");
 
-    hd44780_gotoxy(&lcd, 7, 2);
-    hd44780_puts(&lcd, stations[station_index + 1].radio_name);
+        hd44780_gotoxy(&lcd, 7, 2);
+        hd44780_puts(&lcd, stations[station_index + 1].radio_name);
     }
 
     hd44780_gotoxy(&lcd, 0, 3);
@@ -104,8 +106,10 @@ void radio_selection_menu()
     selected_station = stations[station_index];
 }
 
-void select_next(){
-    if (station_index < (sizeof(stations) / sizeof(stations[0]))){
+void select_next()
+{
+    if (station_index < (sizeof(stations) / sizeof(stations[0])))
+    {
         station_index++;
         // selected_station = stations[station_index];
         radio_selection_menu();
@@ -113,27 +117,38 @@ void select_next(){
     }
 }
 
-void select_previous(){
-    if (station_index > 0){
+void select_previous()
+{
+    if (station_index > 0)
+    {
         station_index--;
         // selected_station = stations[station_index];
-         radio_selection_menu();
-         printf(selected_station.radio_name);
+        radio_selection_menu();
+        printf(selected_station.radio_name);
     }
 }
 
+void disconnect_radio()
+{
+    //delete task
+    vTaskDelete(radio_task_handle);
+
+    radio_selection_menu();
+}
+
 /*
-* Function: initialiseer de inhoud van de "song" pagina
-* Parameters: None
-* Returns: None
-*/
-void song_page_init(){
+ * Function: initialiseer de inhoud van de "song" pagina
+ * Parameters: None
+ * Returns: None
+ */
+void song_page_init()
+{
     current_page = song_selection_page;
 
     song songs[3] = {
-    {1, "brood"},
-    {2, "plankje"},
-    {3, "knakworst"}};
+        {1, "brood"},
+        {2, "plankje"},
+        {3, "knakworst"}};
 
     size_t num_songs = sizeof(songs) / sizeof(songs[0]);
 
@@ -192,14 +207,14 @@ void radio_play_menu()
     hd44780_gotoxy(&lcd, 0, 3);
     hd44780_puts(&lcd, "BACK | X | X | x");
 
-    xTaskCreate(start_radio, "start reader", configMINIMAL_STACK_SIZE * 6, selected_station.url, 5, NULL);
+    xTaskCreate(start_radio, "start reader", configMINIMAL_STACK_SIZE * 6, selected_station.url, 5, &radio_task_handle);
 }
 
 /*
-* Function: initialiseer de setting voor het LCD scherm
-* Parameters: None
-* Returns: None
-*/
+ * Function: initialiseer de setting voor het LCD scherm
+ * Parameters: None
+ * Returns: None
+ */
 void init_lcd()
 {
     hd44780_t lcd_display = {
@@ -217,13 +232,10 @@ void init_lcd()
 
     lcd = lcd_display;
 
-
     memset(&pcf8574, 0, sizeof(i2c_dev_t));
     ESP_ERROR_CHECK(pcf8574_init_desc(&pcf8574, CONFIG_EXAMPLE_I2C_ADDR, 0, CONFIG_EXAMPLE_I2C_MASTER_SDA, CONFIG_EXAMPLE_I2C_MASTER_SCL));
 
-
     ESP_ERROR_CHECK(hd44780_init(&lcd));
-  
 
     hd44780_switch_backlight(&lcd, true);
 
@@ -232,18 +244,19 @@ void init_lcd()
 }
 
 /*
-* Function: initialiseer de paginas met de bijbehorende navigatie
-* Parameters: None
-* Returns: None
-*/
-void initPages(){
-    
+ * Function: initialiseer de paginas met de bijbehorende navigatie
+ * Parameters: None
+ * Returns: None
+ */
+void initPages()
+{
+
     main_page.set_lcd_text = main_menu;
     main_page.button1 = NULL;
     main_page.button2 = input_menu;
     main_page.button3 = radio_page_init;
     main_page.button4 = song_page_init;
-    
+
     input_page.set_lcd_text = input_menu;
     input_page.button1 = NULL;
     input_page.button2 = NULL;
@@ -266,7 +279,7 @@ void initPages(){
     radio_play_page.button1 = NULL;
     radio_play_page.button2 = NULL;
     radio_play_page.button3 = NULL;
-    radio_play_page.button4 = radio_page_init;
+    radio_play_page.button4 = disconnect_radio;
 
     song_play_page.set_lcd_text = song_play_menu;
     song_play_page.button1 = NULL;
@@ -281,11 +294,11 @@ void initPages(){
 uint16_t previous_value = -1;
 
 /*
-* Function: roept alle initialisaties aan en vraagt de waarde op van de knoppen voor het navigeren door het menu
-* Parameters: None
-* Returns: None
-*/
-void getButtonValue(void * parameters)
+ * Function: roept alle initialisaties aan en vraagt de waarde op van de knoppen voor het navigeren door het menu
+ * Parameters: None
+ * Returns: None
+ */
+void getButtonValue(void *parameters)
 {
     init_lcd();
     initPages();
@@ -297,31 +310,36 @@ void getButtonValue(void * parameters)
             previous_value = button_value;
             ESP_LOGI("hans", "retrieved value: %d", button_value);
 
-            switch(button_value){
-                case 1:
-                if (current_page.button1 != NULL) {
+            switch (button_value)
+            {
+            case 1:
+                if (current_page.button1 != NULL)
+                {
                     current_page.button1();
                 }
                 break;
-                case 2:
-                if (current_page.button2 != NULL) {
+            case 2:
+                if (current_page.button2 != NULL)
+                {
                     current_page.button2();
                 }
                 break;
-                case 4:
-                if (current_page.button3 != NULL) {
+            case 4:
+                if (current_page.button3 != NULL)
+                {
                     current_page.button3();
                 }
                 break;
-                case 8:
-                if (current_page.button4 != NULL) {
+            case 8:
+                if (current_page.button4 != NULL)
+                {
                     current_page.button4();
                 }
                 break;
             }
         }
         xSemaphoreGive(xMutex);
-      
+
         vTaskDelay(pdMS_TO_TICKS(50));
     }
 }
