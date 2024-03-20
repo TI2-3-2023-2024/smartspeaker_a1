528051aebb4d79d6daae83c312f0f96d5c39b34d
Thijmeeee <DoeKanaalDigitaal@gmail.com>
2024-03-20 14:20:53 +0100

    Arduino code for frequency functional

--- LED_Display/LED_Display.ino
+++ LED_Display/LED_Display.ino
@@ -9,6 +9,7 @@
 #include <Adafruit_NeoPixel.h>
 #include <Wire.h>
 #include <WireSlave.h>
+#include <FreeRTOSConfig.h>
 #include "driver/i2c.h"
 
 #define SDA_PIN 21
@@ -19,6 +20,11 @@
 #define NUM_PIXELS 256  // The number of LEDs (pixels) on WS2812B LED strip
 
 Adafruit_NeoPixel ws2812b(NUM_PIXELS, PIN_WS2812B, NEO_GRB + NEO_KHZ800);
+
+struct coordinate{
+  uint8_t x;
+  uint8_t y;
+};
 
 
 void setup() {
@@ -54,24 +60,96 @@ void clear_display(){
 */
 void receiveEvent(int howMany)
 {
-  int x;
-  int y;
+  uint8_t x;
+  uint8_t y;
+  char c;
+  struct coordinate* c1;
+
   while(Wire.available()) // loop through all but the last
   {
-    x = Wire.read(); // receive byte as a character
-    Serial.print("x: ");
-    Serial.print(x);         // print the character
+    c = Wire.read(); // read the command of what to do
 
-    Serial.println("");
+    switch (c){
+      case 'b':
+        x = Wire.read(); // receive byte as a character
+        break;
 
-    y = Wire.read(); // receive byte as a character
-     Serial.print("y: ");
-    Serial.print(y);         // print the character
+      case 'c':
+        x = Wire.read(); // receive byte as a character
+        y = Wire.read(); // receive byte as a character
 
-       Serial.println("");
+        c1 = (struct coordinate*)malloc(sizeof(struct coordinate)); // Dynamically allocate memory
+        if (c1 != NULL) {
+          c1->x = x;
+          c1->y = y;
+          xTaskCreate(show_frequency, "show_frequency", 2048, (void *)c1, 5, NULL);
+        }
+       break;
+
+      case 's':
+        x = Wire.read(); //receive byte as a character
+        Serial.print("starting-up");
+        startup_animation();
+        break;
+
+    }
+  }
+}
+
+void show_frequency(void* coordinates) {
+  struct coordinate* c = (struct coordinate*)coordinates;
+
+  Serial.print("x: ");
+  Serial.print(c->x);
+  Serial.print("");
+
+  Serial.print("y: ");
+  Serial.print(c->y);
+  Serial.println("");
+
+  vTaskDelay(pdMS_TO_TICKS(20));
+
+  turn_off_column(c->x);
+  turn_below_point_on(c->x, c->y, 0, 255, 0);
+
+  free(c); // Free dynamically allocated memory
+  vTaskDelete(NULL);
+}
+
+void fall_to_position(int x, int y){
+  for (int i = 7; i >= y; i--) {
+    turn_on_led(x, i, 0, 255, 0);
+    if( x%2==0 && x == 7 ){
+         continue;
+    }
+    turn_off_led(x, i + 1);
+  }
+}
+
+void fall_full_row(int x, int y){
+  for(int i = 0; i <= y; i++){
+    fall_to_position(x, i);
   }
 
-  turn_below_point_on(x, y, 0, 255, 0);
+} 
+
+void startup_animation(){
+  
+  int pixelArray[16];
+  generateArray(pixelArray, 16);
+
+  for (int i = 0; i<16; i++){
+    fall_full_row(i, pixelArray[i]);
+  }
+  delay(1000);
+  ws2812b.clear();
+  ws2812b.show();
+}
+
+void generateArray(int array[], int length) {
+  for (int i = 0; i < length; i++) {
+    array[i] = random(2, 8); // Generates random numbers between 0 and 7
+  }
 }
 
 void turn_off_led(int x, int y){
@@ -84,7 +162,6 @@ void turn_off_led(int x, int y){
     pixel += y;
   }
     ws2812b.setPixelColor(pixel, ws2812b.Color(0, 0, 0));  // it only takes effect if pixels.show() is called
-    ws2812b.setBrightness(50); // set brightness of the pixels
     ws2812b.show();     
 }
 
@@ -101,10 +178,21 @@ void turn_on_led(int x, int y, int r, int g , int b){
   r = (255/7) * y;
   g = 255 - ((255/7) * y);
   b = 0;
+       
+}
+  ws2812b.setPixelColor(pixel, ws2812b.Color(r, g, b));  // it only takes effect if pixels.show() is called
+  ws2812b.show();     
+}
 
-    ws2812b.setPixelColor(pixel, ws2812b.Color(r, g, b));  // it only takes effect if pixels.show() is called
-    ws2812b.setBrightness(50); // set brightness of the pixels
-    ws2812b.show();     
+void set_brightness_of_leds(uint8_t value){
+  ws2812b.setBrightness(value);
+  ws2812b.show();
+}
+
+void turn_off_column(int x){
+  for(int i = 0; i < 8; i++){
+    turn_off_led(x, i);
+  }
 }
 
 void turn_on_row(int y, int r, int g, int b){
